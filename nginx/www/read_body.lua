03d496f93229afbe058f443dee6ae32df62c2985
navid.shokri <shokri@sabacell.ir>
2021-05-11 18:07:21 +0430

    content by lua block

--- nginx/www/read_body.lua
+++ nginx/www/read_body.lua
@@ -1,5 +1,1 @@
---local json = require("cjson")
-ngx.req.read_body();
-local body = ngx.req.get_body_data()
-kafka.sendmessage("DspBidRequestedIntegrationEvent", body)
---ngx.say(json.encode(event));
+
