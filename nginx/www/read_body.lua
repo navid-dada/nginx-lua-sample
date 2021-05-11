927b7a7291256409f1a56ee713dadc328eacabb7
navid.shokri <shokri@sabacell.ir>
2021-05-11 16:17:57 +0430

    bug fix

--- nginx/www/read_body.lua
+++ nginx/www/read_body.lua
@@ -1,5 +1,5 @@
 --local json = require("cjson")
 ngx.req.read_body();
 local body = ngx.req.get_body_data()
-kafka.sendmessage("DspBidRequestedIntegrationEvent", json.encode(event))
+kafka.sendmessage("DspBidRequestedIntegrationEvent", body)
 --ngx.say(json.encode(event));
